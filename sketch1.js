0c2f2a68dc927564fed5599a4baa44e388fd8eb1
Barnaby Collins <barnabyc@sky.com>
2018-11-20 10:41:37 +0000

    minion sketch

--- sketch1.js
+++ sketch1.js
@@ -1,7 +1,13 @@
+var minionGOD;
+
+function preload() {
+    minionGOD = loadImage('magnus.png');
+}
+
 function setup() {
     var myCanvas = createCanvas(640, 480);
     myCanvas.parent("canvascontainer");
 }
 function draw() {
-    
+    image(minionGOD, mouseX, mouseY);
 }
