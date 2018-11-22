ab4436f02d91b2c26aeb6b3007b7ab44da358659
Barnaby Collins <barnabyc@sky.com>
2018-11-22 11:54:31 +0000

    Finish initial 2D project

--- sketch1.js
+++ sketch1.js
@@ -1,13 +1,16 @@
-var minionGOD;
+//var minionGOD;
 
 function preload() {
-    minionGOD = loadImage('magnus.png');
+    //minionGOD = loadImage('magnus.png');
 }
 
 function setup() {
     var myCanvas = createCanvas(640, 480);
     myCanvas.parent("canvascontainer");
+    stroke('#00d2ff');
+    fill(0);
 }
 function draw() {
-    image(minionGOD, mouseX, mouseY);
+    ellipse(mouseX, mouseY, 50, 50);
+    //image(minionGOD, mouseX, mouseY);
 }
