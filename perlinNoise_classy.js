68406e4a64eb5a8c0cfd873aa238d67eabb1e993
Barnaby Collins <barnabyc@sky.com>
2018-12-31 17:52:43 +0000

    First attempt at turning it into a class

--- perlinNoise_classy.js
+++ perlinNoise_classy.js
@@ -1,140 +1,148 @@
 // This sketch borrows heavily from yasai's perlin noise sketch
 // Tony R. 2018
 
-var seed = 'cheeses';       // seed for use by random() and noise()
-var nums;                   // number of particles to instantiate
-var minLife = 0;            // minimum life for each particle
-var maxLife = 10;           // maximum life for each particle
-var noiseScale = 200;       // constant to scale noise with
-var	simulationSpeed = 0.2;  // constant to scale particle velocities with
-var fadeFrame = 0;          // iterating variable to count frames
+class PerlinNoise {
+    constructor (parentDiv, width, height, seed, numParticles, minLife, maxLife, noiseScale, speed, paddingY, paddingX, backgroundColour, defaultColour, colourL, colourR) { 
+        this.parent = parentDiv || false;                               // id of div to be used as parent (false if none)
+        this.width = width || windowHeight;                             // width of canvas
+        this.height = height || windowHeight;                           // height of canvas
+        this.seed = seed || 'cheeses';                                  // seed for use by random() and noise()
+        this.nums = numParticles || 100;                                // number of particles to instantiate
+        this.minLife = minLife || 0;                                    // minimum life for each particle
+        this.maxLife = maxLife || 10;                                   // maximum life for each particle
+        this.noiseScale = noiseScale || 200;                            // constant to scale noise with
+        this.simulationSpeed = speed || 0.2;                            // constant to scale particle velocities with
+        this.fadeFrame = 0;                                             // iterating variable to count frames
+        this.padding_top = paddingY || 20;                              // padding on top & bottom of box
+        this.padding_side = paddingX || 20;                             // padding on sides of box
+        this.particles = [];                                            // array to put particles in
+        this.backgroundColor = backgroundColour || color(20, 20, 20);   // background colour
+        this.defaultColour = defaultColour || color('white');           // colour for non-coloured particles
+        this.color_from = colourR || color('purple');                   // colour for moving right
+        this.color_to = colourL || color('cyan');                       // colour for moving left
 
-var padding_top = 20;       // padding on top of box
-var padding_side = 20;      // padding on bottom of box
+        var myCanvas = createCanvas(this.width, this.height);
+        if (parent) {
+            myCanvas.parent(this.parent);
+        }
+        element = document.getElementById('p5_loading');
+        element.parentNode.removeChild(element);
 
-var particles = [];         // array to put particles in
-var backgroundColor;        // background colour
-var color_from;             // colour for moving right
-var color_to;               // colour for moving left
+        randomSeed(this.seed);
+        noiseSeed(this.seed);
+
+        background(this.backgroundColor);
+        
+        noStroke();
+        smooth();
+        
+        // generate initial particle locations
+        for(var i = 0; i < nums; i++){
+            var p = new Particle(this.minLife, this.maxLife, this.defaultColour, this.color_to, this.color_from, this.width, this.height, this.padding_side, this.padding_top, this.noiseScale, this.simulationSpeed);
+            p.pos.x = random(this.padding_side, this.width-this.padding_side);
+            p.pos.y = this.padding_top;
+            particles[i] = p;
+        }
+        
+        fill(color(255));
+    } // end constructor
+
+    draw() {
+        ++this.fadeFrame;               // increment fadeFrame
+        if(this.fadeFrame % 5 == 0){    // every 5th frame
+            
+            blendMode(DIFFERENCE);      // fade past particle trails
+            fill(1, 1, 1);
+            rect(0,0,this.width,this.height);
+    
+            blendMode(LIGHTEST);
+            fill(this.backgroundColor);
+            rect(0,0,this.width,this.height);
+        }
+        
+        blendMode(BLEND);
+        
+        // iterate through particles
+        for(var i = 0; i < this.nums; i++){
+            // iterations and radius both dependent on the particle's index
+            // (as they are randomly placed this ensures we have an even spread without creating patterns)
+            var iterations = map(i,0,this.nums,5,1);
+            var radius = map(i,0,this.nums,2,6);
+            
+            this.particles[i].move(iterations);
+            this.particles[i].checkEdge();
+            
+            var alpha = 255;
+            
+            // work out the colour the particle should have based on its heading
+            var particle_heading = this.particles[i].vel.heading()/PI;
+            if(particle_heading < 0){
+                particle_heading *= -1;
+            }
+            var particle_color = lerpColor(this.particles[i].color1, this.particles[i].color2, particle_heading);
+            
+            // work out how bright (opaque) the particle should be based on its life:
+            // dark initially and finally, and bright otherwise
+            var fade_ratio;
+            fade_ratio = min(particles[i].life * 5 / maxLife, 1);
+            fade_ratio = min((maxLife - particles[i].life) * 5 / maxLife, fade_ratio);
+    
+            // show the particle now that colour etc has been processed
+            fill(red(particle_color), green(particle_color), blue(particle_color), alpha * fade_ratio);
+            particles[i].display(radius);
+        }
+    }
+}
 
 function windowResized() {
     resizeCanvas(windowWidth * 0.4, windowHeight);
 }
 
-function setup(){
-    element = document.getElementById('p5_loading');
-    element.parentNode.removeChild(element);
-    randomSeed(seed);
-    noiseSeed(seed);
-    nums = 100;
-    
-    backgroundColor = color(20, 20, 20);
-    //color_from = color('black');
-    //color_to = color('white');
-    color_from = color('purple');
-    color_to = color('cyan');
-    
-    //createCanvas(windowWidth, windowHeight);
-    var myCanvas = createCanvas(windowWidth * 0.4, windowHeight);
-    myCanvas.parent('canvascontainer');
-    background(backgroundColor);
-    
-    noStroke();
-    smooth();
-    
-    for(var i = 0; i < nums; i++){
-        var p = new Particle();
-        p.pos.x = random(padding_side, width-padding_side);
-        p.pos.y = padding_top;
-        particles[i] = p;
-    }
-    
-    background(color(0));
-    fill(color(255));
-}
-
-function draw(){
-    
-    ++fadeFrame;
-    if(fadeFrame % 5 == 0){
-        
-        blendMode(DIFFERENCE);
-        fill(1, 1, 1);
-        rect(0,0,width,height);
-
-        blendMode(LIGHTEST);
-        //blendMode(DARKEST); //looks terrible. stutters
-        fill(backgroundColor);
-        rect(0,0,width,height);
-    }
-    
-    blendMode(BLEND);
-    
-    for(var i = 0; i < nums; i++){
-        var iterations = map(i,0,nums,5,1);
-        var radius = map(i,0,nums,2,6);
-        
-        particles[i].move(iterations);
-        particles[i].checkEdge();
-        
-        var alpha = 255;
-        
-        var particle_heading = particles[i].vel.heading()/PI;
-        if(particle_heading < 0){
-            particle_heading *= -1;
-        }
-        var particle_color = lerpColor(particles[i].color1, particles[i].color2, particle_heading);
-        
-        var fade_ratio; //TODO
-        fade_ratio = min(particles[i].life * 5 / maxLife, 1);
-        fade_ratio = min((maxLife - particles[i].life) * 5 / maxLife, fade_ratio);
-
-        fill(red(particle_color), green(particle_color), blue(particle_color), alpha * fade_ratio);
-        particles[i].display(radius);
-    } 
-}
-
-
-
-
-
-
-function Particle(){
+function Particle(minLife, maxLife, defaultColour, colourL, colourR, width, height, padding_side, padding_top, noiseScale, simulationSpeed){
     // member properties and initialization
     this.vel = createVector(0, 0);
     this.pos = createVector(random(0, width), random(0, height));
     this.life = random(minLife, maxLife);
     this.flip = int(random(0,2)) * 2 - 1;
-    this.color1 = this.color2 = color('white');
+    this.color1 = this.color2 = defaultColour;
     
+    // at a 1/3 chance, make this a coloured particle
     if(int(random(3)) == 1){
         //this.color1 = color('palegreen');
         //this.color2 = color('cyan');
-        this.color1 = color_from;
-        this.color2 = color_to;
+        this.color1 = colourR;
+        this.color2 = colourL;
     }
     
     // member functions
     this.move = function(iterations){
+        // if dead, respawn
         if((this.life -= 0.01666) < 0) {
             this.respawnTop();
         }
-        while(iterations > 0){
+
+        // while we still have iterations to use up
+        while(iterations > 0) {
             
+            // compute angle
             var transition = map(this.pos.x, padding_side, width-padding_side, 0, 1);
             var angle = noise(this.pos.x/noiseScale, this.pos.y/noiseScale)*transition*TWO_PI*noiseScale;
             //var transition = map(this.pos.y, height/5, height-padding_top, 0, 1, true);
             //var angle = HALF_PI;
             //angle += (noise(this.pos.x/noiseScale, this.pos.y/noiseScale)-0.5)*transition*TWO_PI*noiseScale/66;
 
+            // work out the position we will be at as a result of this angle
             this.vel.x = cos(angle);
             this.vel.y = sin(angle);
             this.vel.mult(simulationSpeed);
             this.pos.add(this.vel);
+
+            // decrement iterations
             --iterations;
         }
     };
 
+    // respawn if we're near the edge
     this.checkEdge = function(){
         if(this.pos.x > width - padding_side
         || this.pos.x < padding_side
@@ -144,20 +152,21 @@ function Particle(){
         }
     };
     
+    // unused alternative respawn function where we respawn anywhere in the canvas, not necessarily at the top
     this.respawn = function(){
         this.pos.x = random(0, width);
         this.pos.y = random(0, height);
         this.life = maxLife;
     };
     
+    // respawn at the top
     this.respawnTop = function() {
         this.pos.x = random(padding_side, width-padding_side);
         this.pos.y = padding_top;
         this.life = maxLife;
-        //this.color1 = lerpColor(color('white'), color_from, (this.pos.x-padding_side)/inner_square);
-        //this.color2 = lerpColor(color('white'), color_to, (this.pos.x-padding_side)/inner_square);
     };
 
+    // actually show an ellipse at the position
     this.display = function(r){
         ellipse(this.pos.x, this.pos.y, r, r);
     };
