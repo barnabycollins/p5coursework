be043b917339625239b30755b30bf3c43b853f04
Barnaby Collins <barnabyc@sky.com>
2019-01-03 17:54:46 +0000

    Add second button to demo

--- demo.js
+++ demo.js
@@ -1,7 +1,7 @@
-var pNoise, seed = 759, numParticles = 250, mode, minLife, maxLife = 20, noiseScale, speed, paddingY, paddingX, backgroundColour, defaultColour, colourL, colourR, submit;
+var pNoise, seed = 759, numParticles = 250, mode, minLife, maxLife = 20, noiseScale, simulationSpeed, paddingY, paddingX, backgroundColour, defaultColour, colourL, colourR, submit;
 
 function setup() {
-    pNoise = new PerlinNoise('canvascontainer', windowWidth * 0.4, windowHeight, seed, numParticles, mode, minLife, maxLife, noiseScale, speed, paddingY, paddingX, backgroundColour, defaultColour, colourL, colourR);
+    pNoise = new PerlinNoise('canvascontainer', windowWidth * 0.4, windowHeight, seed, numParticles, mode, minLife, maxLife, noiseScale, simulationSpeed, paddingY, paddingX, backgroundColour, defaultColour, colourL, colourR);
 }
 function draw() {
     pNoise.draw();
@@ -10,7 +10,34 @@ function windowResized() {
     pNoise.canvasSize(windowWidth * 0.4, windowHeight);
 }
 
+/**
+ * Update the (live) sketch with new values from the form
+ */
 function updateSketch() {
+    for (i = 0; i < inputs.length; i++) {
+        // for every input
+        if (inputs[i].className.indexOf('number') !== -1) {
+            // for numbers
+            if (!isNaN(eval('Number(form.' + inputs[i].name + '.value);'))) {
+                // if it's a valid number
+                eval('pNoise.setParameter("' + inputs[i].name + '", Number(form.' + inputs[i].name + '.value));');
+            }
+        }
+        else if (inputs[i].className == 'colour') {
+            // for colours
+            eval('pNoise.setParameter("' + inputs[i].name + '", color(form.' + inputs[i].name + '.value));');
+        }
+        else if (inputs[i].className !== 'submit-button') {
+            // for strings (none implemented in the demo at the moment)
+            eval('pNoise.setParameter("' + inputs[i].name + '", form.' + inputs[i].name + '.value);');
+        }
+    }
+}
+
+/**
+ * Restart sketch with new values from the form
+ */
+function restartSketch() {
     for (i = 0; i < inputs.length; i++) {
         if (inputs[i].className.indexOf('number') !== -1) {
             if (isNaN(eval('Number(form.' + inputs[i].name + '.value);'))) {
@@ -23,7 +50,7 @@ function updateSketch() {
         else if (inputs[i].className == 'colour') {
             eval(inputs[i].name + ' = color(form.' + inputs[i].name + '.value);');
         }
-        else {
+        else if (inputs[i].className !== 'submit-button') {
             eval(inputs[i].name + ' = form.' + inputs[i].name + '.value;');
         }
     }
@@ -36,5 +63,8 @@ document.addEventListener('DOMContentLoaded', function() {
     form.addEventListener('submit', function(event) {
         event.preventDefault();
         updateSketch();
+    });
+    document.getElementById('restart-btn').addEventListener('click', function() {
+        restartSketch();
     });
 });
