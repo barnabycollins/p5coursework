e91b00c14fd84abb271759ac253fd8e0173c2461
Barnaby Collins <barnabyc@sky.com>
2019-01-06 17:51:40 +0000

    Revert "Retaining useless work before I revert"
    This reverts commit 89e7f9db37169b9ed069e72f60baf68b75da465d.

--- demo.js
+++ demo.js
@@ -1,34 +1,14 @@
-var element, canvas, pNoise, seed = 759, numParticles = 250, mode, minLife, maxLife = 20, noiseScale, simulationSpeed, paddingY, paddingX, defaultColour, colourL, colourR, submit;
+var pNoise, seed = 759, numParticles = 250, mode, minLife, maxLife = 20, noiseScale, simulationSpeed, paddingY, paddingX, defaultColour, colourL, colourR, submit;
 
 function setup() {
-    element = document.getElementById('p5_loading');
-    if (element) {
-        element.parentNode.removeChild(element);
-    }
-
-
-    canvas = createCanvas(windowWidth * 0.4, windowHeight);
-    canvas.parent('canvascontainer');
-    pNoise = new PerlinNoise(canvas, seed, numParticles, mode, minLife, maxLife, noiseScale, simulationSpeed, paddingY, paddingX, defaultColour, colourL, colourR);
+    pNoise = new PerlinNoise('canvascontainer', windowWidth * 0.4, windowHeight, seed, numParticles, mode, minLife, maxLife, noiseScale, simulationSpeed, paddingY, paddingX, defaultColour, colourL, colourR);
 }
 function draw() {
-    pNoise.draw(canvas);
+    pNoise.draw();
 }
 function windowResized() {
-    resizeCanvas(windowWidth * 0.4, windowHeight);
+    pNoise.canvasSize(windowWidth * 0.4, windowHeight);
 }
-
-document.addEventListener('DOMContentLoaded', function() {
-    form = document.getElementById('parameterform');
-    inputs = form.getElementsByTagName('input');
-    form.addEventListener('submit', function(event) {
-        event.preventDefault();
-        updateSketch();
-    });
-    document.getElementById('restart-btn').addEventListener('click', function() {
-        restartSketch();
-    });
-});
 
 /**
  * Update the (live) sketch with new values from the form
@@ -76,3 +56,15 @@ function restartSketch() {
     }
     setup();
 }
+
+document.addEventListener('DOMContentLoaded', function() {
+    form = document.getElementById('parameterform');
+    inputs = form.getElementsByTagName('input');
+    form.addEventListener('submit', function(event) {
+        event.preventDefault();
+        updateSketch();
+    });
+    document.getElementById('restart-btn').addEventListener('click', function() {
+        restartSketch();
+    });
+});
